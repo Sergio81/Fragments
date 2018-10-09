904b0cd67d9ffb89e147e5e8076874d85179d741
Sergio Fabian Aguilar Vega <sergiofav@outlook.com>
2018-10-09 10:50:57 -0400

    Initial commit

---
+++ app/src/main/java/com/androidtrainin/fragments/MainActivity.java
@@ -0,0 +1,22 @@
+package com.androidtrainin.fragments;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.container, new BlankFragment())
+                .commit();
+
+    }
+}
